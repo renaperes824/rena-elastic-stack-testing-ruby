b01b4491cbcf6d65711f15601c4cdff1a28c8c5b
Rena Peres <rena.peres824@gmail.com>
2021-05-18 16:42:26 -0600

    Increase Kibana RAM for reporting tests (#904)

--- ci/upgrade/buildSrc/src/main/java/org/estf/gradle/CreateEssDeployment.java
+++ ci/upgrade/buildSrc/src/main/java/org/estf/gradle/CreateEssDeployment.java
@@ -242,7 +242,7 @@ public class CreateEssDeployment extends DefaultTask {
         KibanaClusterTopologyElement kbnTopology = new KibanaClusterTopologyElement()
                 .instanceConfigurationId(kbnInstanceCfg)
                 .zoneCount(1)
-                .size(getTopologySize(2048));
+                .size(getTopologySize(4096));
 
         KibanaConfiguration kbnCfg = new KibanaConfiguration()
                 .version(stackVersion);
