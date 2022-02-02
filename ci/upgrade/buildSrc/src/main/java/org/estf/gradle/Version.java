3acef0feaa623f4440ced9eb93031c52149e281b
Rena Peres <rena.peres824@gmail.com>
2022-02-02 14:56:41 -0700

    Fix versions for security solutions (#1123)

--- ci/upgrade/buildSrc/src/main/java/org/estf/gradle/Version.java
+++ ci/upgrade/buildSrc/src/main/java/org/estf/gradle/Version.java
@@ -12,6 +12,9 @@ public class Version {
             throw new IllegalArgumentException("Version can not be null");
         if (!version.matches("[0-9]+(\\.[0-9]+)*"))
             throw new IllegalArgumentException("Invalid version format");
+        if (version.contains("SNAPSHOT")) {
+            version = version.replaceAll("-SNAPSHOT", "");
+        }
         this.version = version;
     }
 
@@ -32,3 +35,4 @@ public class Version {
         return true;
     }
 }
+
