9dfba37066d87790ae985607c2e5dfb52af48d37
Rena Peres <rena.peres824@gmail.com>
2022-03-31 15:48:31 -0600

    Update scripted fields API to runtime fields (#1207)

--- ci/upgrade/buildSrc/src/main/java/org/estf/gradle/ScriptedFieldsApi.java
+++ ci/upgrade/buildSrc/src/main/java/org/estf/gradle/ScriptedFieldsApi.java
@@ -3,6 +3,7 @@ package org.estf.gradle;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.util.EntityUtils;
+import org.apache.maven.artifact.versioning.ComparableVersion;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.TaskAction;
@@ -31,14 +32,11 @@ public class ScriptedFieldsApi extends DefaultTask {
     public String password;
 
     @Input
-    public String version;
-
-    @Input
-    public String upgradeVersion;
+    public String finalVersion;
 
     @TaskAction
     public void run() {
-        RestApi api = new RestApi(username, password, version, upgradeVersion);
+        RestApi api = new RestApi(username, password, null, null);
         updateScriptedField(api);
         System.out.println("Update scripted fields completed successfully");
     }
@@ -56,25 +54,43 @@ public class ScriptedFieldsApi extends DefaultTask {
             JSONArray saved_objects = json.getJSONArray("saved_objects");
             JSONObject elem = saved_objects.getJSONObject(0);
             return elem.getString("id");
-        } catch (Exception ignored) {}
+        } catch (Exception ignored) {
+            System.out.println(ignored);
+        }
         return "";
     }
 
     public void updateScriptedField(RestApi api) {
+        ComparableVersion currentVersion = new ComparableVersion(finalVersion);
+        ComparableVersion version7_17 = new ComparableVersion("7.17.0");
+        String field_path = "/scripted_field/hour_of_day";
+        String json_body = "{\"field\": {\"script\": \"doc['timestamp'].value.getHour()\"}}";
+        if (currentVersion.compareTo(version7_17) >= 0) {
+            field_path = "/runtime_field/hour_of_day";
+            json_body = "{\n" +
+                    "  \"runtimeField\": {\n" +
+                    "     \"script\": {\n" +
+                    "        \"source\": \"emit(doc[\\\"timestamp\\\"].value.getHour())\"\n" +
+                    "      }\n" +
+                    "  }\n" +
+                    "}";
+        }
         try {
             String[] ids = {this.getIndexPatternId(api, "kibana_sample_data_flights", ""),
                             this.getIndexPatternId(api, "kibana_sample_data_logs", ""),
                             this.getIndexPatternId(api, "kibana_sample_data_flights", "automation"),
                             this.getIndexPatternId(api, "kibana_sample_data_logs", "automation")};
             for (int i = 0; i < ids.length; i++) {
-                String path = "/api/index_patterns/index_pattern/" + ids[i] + "/scripted_field/hour_of_day";
+                String path = "/api/index_patterns/index_pattern/" + ids[i] + field_path;
                 if (i>=2) {
-                    path = "/s/automation/api/index_patterns/index_pattern/" + ids[i] + "/scripted_field/hour_of_day";
+                    path = "/s/automation/api/index_patterns/index_pattern/" + ids[i] + field_path;
                 }
                 api.post(kbnBaseUrl + path,
-                        "{\"field\": {\"script\": \"doc['timestamp'].value.getHour()\"}}",
+                        json_body,
                         true);
             }
-        } catch (Exception ignored) {}
+        } catch (Exception ignored) {
+            System.out.println(ignored);
+        }
     }
 }
