f8e16db4b70a634949807d7a1742116bae69eb25
Rena Peres <rena.peres824@gmail.com>
2022-05-19 14:28:34 -0600

    Update build_funcs.sh

--- scripts/shell/lib/build_funcs.sh
+++ scripts/shell/lib/build_funcs.sh
@@ -480,7 +480,7 @@ run_cloud_tests() {
   fi
 
   # Going to also trigger Buildkite job for CKFT, jenkins job TBR after testing
-  if [[ $TASK == "saas_run_kibana_tests" ]]; then
+  if [[ $TASK == "saas_run_kibana_tests" ]] && [[ $JOB == "basicGrp1" ]]; then
     bktoken=$(vault read secret/stack-testing/estf-bk-token -format=json | jq -r .data.token)
     curl -sH "Authorization: Bearer $bktoken" "https://api.buildkite.com/v2/organizations/elastic/pipelines/estf-cloud-kibana-functional-tests/builds" \
          -X "POST" \
